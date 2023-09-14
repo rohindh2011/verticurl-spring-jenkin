33388e4824a2925872e46eb3db9ae7ab7cbf2b43
Rohindh-VIN2081 <rohindhkishore.marimuthu@verticurl.com>
2023-09-14 12:50:03 +0530

    first commit

---
+++ src/main/java/com/verticurl/jenkins/springjenkinsproject/SpringJenkinsProjectApplication.java
@@ -0,0 +1,14 @@
+package com.verticurl.jenkins.springjenkinsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsProjectApplication.class, args);
+		System.out.println("Welcome to Verticurl");
+	}
+
+}
---
+++ src/test/java/com/verticurl/jenkins/springjenkinsproject/SpringJenkinsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verticurl.jenkins.springjenkinsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
